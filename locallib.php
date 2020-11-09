1fd939a8d0d0b9d8ec94bda0fa9fcf699c422dd7
Charles Fulton <fultonc@lafayette.edu>
2020-11-09 10:11:44 -0500

    Document function; fixes #20

--- locallib.php
+++ locallib.php
@@ -29,6 +29,11 @@ define('COURSE_MERGE_DEPTH_SAME_CATEGORY', 1);
 define('COURSE_MERGE_DEPTH_SAME_PARENT', 2);
 define('COURSE_MERGE_DEFAULT_CATEGORY', -1);
 
+/**
+ * Get the advanced fields for the course merge tool.
+ *
+ * @return array
+ */
 function local_course_merge_get_advanced_settings() {
     $raw = get_config('local_course_merge', 'advancedsettings');
     $exploded = explode("\n", $raw);
